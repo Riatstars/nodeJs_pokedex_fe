8cfc9f39a86c8a6b313c04de997f63316c829f16
riatstars <riatstars@gmail.com>
2023-12-27 20:04:40 +0700

    minor commit

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_API;
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_API || "https://pokedex-api-fx8z.onrender.com";
 export const POKEMONS_PER_PAGE = 20;
